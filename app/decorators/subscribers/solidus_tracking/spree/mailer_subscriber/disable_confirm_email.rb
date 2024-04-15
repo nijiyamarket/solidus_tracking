512b8d8123425c8536298b38017cfaff9017385e
rabbitbike <seijiku@hotmail.com>
2024-04-15 14:52:23 -0700

    Remove order_finalized override

--- app/decorators/subscribers/solidus_tracking/spree/mailer_subscriber/disable_confirm_email.rb
+++ app/decorators/subscribers/solidus_tracking/spree/mailer_subscriber/disable_confirm_email.rb
@@ -6,13 +6,13 @@ module SolidusTracking
       module DisableConfirmEmail
         def self.prepended(base)
           base.module_eval do
-            alias_method :original_order_finalized, :order_finalized
+            # alias_method :original_order_finalized, :order_finalized
 
-            def order_finalized(event)
-              return if SolidusTracking.configuration.disable_builtin_emails
-
-              original_order_finalized(event)
-            end
+            # def order_finalized(event)
+            #   return if SolidusTracking.configuration.disable_builtin_emails
+            #
+            #   original_order_finalized(event)
+            # end
           end
         end
       end
